7401d9339f63c6cb5f3ecf39032cf8f42a217a06
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-01-25 09:16:24 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,33 +10,34 @@ const Hero = () => {
     seconds: 0,
   });
 
-  useEffect(() => {
-    const targetDate = new Date();
-    targetDate.setHours(11, 0, 0, 0);
-    if (new Date() > targetDate) {
-      targetDate.setDate(targetDate.getDate());
-    }
-
-    const now = new Date();
-    const timeDifference = targetDate - now;
-    if (timeDifference <= 0) {
-      setTimeLeft({
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      });
-      return;
-    }
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor(
-      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-    setTimeLeft({ days, hours, minutes, seconds });
+   useEffect(() => {
+    const calculateTimeLeft = () => {
+      const targetDate = new Date();
+      targetDate.setHours(11, 0, 0, 0);
+      if (new Date() > targetDate) {
+        targetDate.setDate(targetDate.getDate() + 1); 
+      }
+      const now = new Date();
+      const timeDifference = targetDate - now;
+      if (timeDifference <= 0) {
+        setTimeLeft({
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        });
+        return;
+      }
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+      setTimeLeft({ days, hours, minutes, seconds });
+    };
   }, []);
 
   return (
